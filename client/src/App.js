d071fa20c98efe228331f86cbb8d986162ac6db9
sossw1 <sossw1@gmail.com>
2021-04-01 20:16:29 -0400

    Moved react files to client folder

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import M from 'materialize-css';
+
+class App extends Component {
+  componentDidMount() {
+    M.AutoInit();
+  }
+
+  render() {
+    return(
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
