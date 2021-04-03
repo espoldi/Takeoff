6714b4dc28fe9152a791a76c4cae1825366f71b1
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-03 12:51:32 -0400

    added navbar to page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// Components
+import Navbar from '../components/Navbar.js';
 import Current from '../components/Current.js';
 import Archive from '../components/Archive.js';
 
 function Dashboard() {
     return (
         <>
+            <Navbar />
             <Current />
             <Archive />
         </>
