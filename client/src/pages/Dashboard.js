f90865b858649848c0d36366baddffbe757e5856
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-03 12:24:21 -0400

    added starter code imports, function and export

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Current from '../components/Current.js';
+import Archive from '../components/Archive.js';
+
+function Dashboard() {
+    return (
+        <>
+            <Current />
+            <Archive />
+        </>
+    );
+}
+
+export default Dashboard;
