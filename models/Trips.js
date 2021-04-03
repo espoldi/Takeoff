ddcb1767a4718e5f80f61bd26daa58e218baa137
sossw1 <sossw1@gmail.com>
2021-04-03 17:58:27 -0400

    Added schema properties

--- models/Trips.js
+++ models/Trips.js
@@ -1,9 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
 const TripSchema = new Schema({
-
+  tripName: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    // element of array
+    enum: ['current','complete','bucketList']
+  },
 });
 
-module.exports = Trips = mongoose.model("trips", TripSchema);
+module.exports = Trips = mongoose.model('trips', TripSchema);
