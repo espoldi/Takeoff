278039d0d79956c94a35ab3960e9f640c8385ba1
Nardin Lachowski <nardin@live.com>
2021-04-03 14:16:04 -0400

    including express server setup, passport setup and mongoDB connection setup

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
 require('dotenv').config();
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import passport from 'passport';
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+const users = require('./routes/api/users');
 
 //Express Setup
 const app = express();
-app.use(express.static (__dirname));
+//app.use(express.static (__dirname));
 
-const expressSession = require('express-session')({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-});
+// const expressSession = require('express-session')({
+//     secret: 'secret',
+//     resave: false,
+//     saveUninitialized: false
+// });
 
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ extended: false}));
-app.use(expressSession);
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+//app.use(expressSession);
+//app.use(cors());
 
-//passport setup
-app.use(passport.initialize());
-app.use(passport.session());
+//db config
+const db = require("./config/keys").mongoURI;
 
-//mongoose setup
-const CONNECTION_URL = 'mongodb+srv://herokuTakeoff:nardin5heroku@cluster0.84m2b.mongodb.net/Cluster0?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then (() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch ((error) => console.log(error.message));
+//connection to mongoDB
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then (() => console.log("MongoDB successfully connected"))
+    .catch ((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
 
-const port = process.env.PORT || 5000;
+//passport middleware setup
+app.use(passport.initialize());
+//app.use(passport.session());
 
-app.listen(port, () => console.log(`Server listening on port ${port} !`));
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+//setting port for server use
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT} !`));
