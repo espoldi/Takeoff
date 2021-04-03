901d366ed887d0cc0b09ad24206c8c67a43aadea
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-03 16:44:48 -0400

    converted from a card to the grid system on paper

--- client/src/components/Current.js
+++ client/src/components/Current.js
@@ -2,31 +2,50 @@ import React from 'react';
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-import { Card } from '@material-ui/core/';
-import { CardActions } from '@material-ui/core/';
-import { CardContent } from '@material-ui/core/';
+import { Grid } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import { Button } from '@material-ui/core/';
 
-const useStyles = makeStyles({
-    root: { minwidth: 275 },
-    title: { fontSize: 14 }
-});
+const useStyles = makeStyles((theme) => ({
+    root: { flexGrow: 1 },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary
+    }
+}));
 
 function Current() {
     const classes = useStyles();
     return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary">My Current Adventure</Typography>
-                <Typography component="h2">Destination: </Typography>
-                <Typography component="h3">Start Date: </Typography>
-                <Typography component="h3">End Date: </Typography>
-                <Typography component="h3">Current Activity: </Typography>
-            </CardContent>
+        <Paper className={classes.paper}>
+            <Grid container spacing={1}>
+                <Grid item xs>
+                    <Typography variant="h5" color="textSecondary">My Current Adventure</Typography>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs>
+                    <Typography variant="h4">Destination: </Typography>
+                </Grid>
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid item xs>
+                    <Typography variant="h5">Start Date: </Typography>
+                </Grid>
+                <Grid item xs>
+                    <Typography variant="h5">End Date: </Typography>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+                <Grid item xs>
+                    <Typography variant="h5">Current Activity: </Typography>
+                </Grid>
+            </Grid>
             <CardActions>
-                <Button size="small">Edit Itinerary</Button>
+                <Button>Edit Itinerary</Button>
             </CardActions>
-        </Card>
+        </Paper>
     );
 }
 
