815b5845a53c1bbd9aee67b2fc941e20fbb0d525
sossw1 <sossw1@gmail.com>
2021-04-03 17:27:06 -0400

    Trip model

---
+++ models/Trips.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const TripSchema = new Schema({
+
+});
+
+module.exports = Trips = mongoose.model("trips", TripSchema);
