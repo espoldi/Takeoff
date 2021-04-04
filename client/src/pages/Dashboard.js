a080080502f65c8b262a0861e433657341b3496d
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-04-04 11:16:20 -0400

    imported routes for login, register. used temp navbar to get client running. might want to change image of login

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar.js';
 import Current from '../components/Current.js';
 import Archive from '../components/Archive.js';
 
+
 function Dashboard() {
     return (
         <>
             <Navbar />
             <Current />
             <Archive />
+           
         </>
     );
 }
