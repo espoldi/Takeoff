978d8d5db9e9af4b891c613359ee60912f0b6806
sossw1 <sossw1@gmail.com>
2021-04-05 19:56:54 -0400

    Added properties

--- models/ItineraryItem.js
+++ models/ItineraryItem.js
@@ -3,7 +3,23 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const ItineraryItemSchema = new Schema({
-
+  start: {
+    type: Date,
+    required: true
+  },
+  end: {
+    type: Date,
+    required: true
+  },
+  activity: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+    //ex: ['transportation, eating, sightseeing, social, downtime']
+  }
 });
 
 module.exports = ItineraryItem = mongoose.model('itineraryItems', ItineraryItemSchema);
