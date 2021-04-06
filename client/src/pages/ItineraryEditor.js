d50ff23c19323638f5519e4cb14a3fb25afc14b0
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-05 21:07:41 -0400

    added navbar and container set up

--- client/src/pages/ItineraryEditor.js
+++ client/src/pages/ItineraryEditor.js
@@ -1,8 +1,16 @@
 import React from 'react';
+// Components
+import Navbar from '../components/Navbar.js';
+// Material UI
+import { Container } from '@material-ui/core';
 
 export default function ItineraryEditor() {
     return(
         <>
+            <Navbar />
+            <Container fixed>
+                
+            </Container>
         </>
     );
 }
