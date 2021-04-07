313c3b7d9bed5430078abbb778b43877ab3bcd74
amo02008 <adrienne.osorio@gmail.com>
2021-04-07 15:00:32 -0400

    added starter code for responsive nav

---
+++ client/src/components/ResponsiveNav.js
@@ -0,0 +1,169 @@
+import React from "react";
+// Components
+
+// Material UI
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { AppBar } from "@material-ui/core/";
+import { Toolbar } from "@material-ui/core/";
+import { Typography } from "@material-ui/core/";
+import { IconButton } from "@material-ui/core/";
+import PropTypes from "prop-types";
+import { CssBaseline } from "@material-ui/core/";
+import  {Divider } from "@material-ui/core/";
+import  {Drawer } from "@material-ui/core/";
+import  {Hidden } from "@material-ui/core/";
+import  {List } from "@material-ui/core/";
+import  {ListItem } from "@material-ui/core/";
+import  {ListItemText } from "@material-ui/core/";
+import  {FlightTakeoffIcon } from "@material-ui/icons/";
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+// import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
+
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  // const handleDrawerClose = () => {
+  //   setMobileOpen(false);
+  // };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {["Dashboard", "Itineary Creator", "My Blog", "Bucket List"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+      <Divider />
+      <List>
+        {["Meet the Team", "Contact US", "Log Off"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <FlightTakeoffIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Takeoff
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="dashboard list">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {/* <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div> */}
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+      </main>
+    </div>
+  );
+}
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func
+};
+
+export default ResponsiveDrawer;
