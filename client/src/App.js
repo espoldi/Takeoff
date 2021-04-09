54d4c5e1ef28efad31d4a3ffaefcdc3e863fe59e
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-08 21:38:10 -0400

    added itinerary route to router

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import ItineraryCreator from './pages/ItineraryCreator';
 
 class App extends Component {
   componentDidMount() {
@@ -28,19 +29,25 @@ class App extends Component {
               <li>
                 <Link to="/register">Register</Link>
               </li>
+              <li>
+                <Link to="/creator">Itinerary Creator</Link>
+              </li>
             </ul>
           </nav>
   
           
           <Switch>
-            <Route path="/login">
+            <Route exact path="/login">
               <Login />
             </Route>
-            <Route path="/register">
+            <Route exact path="/register">
               <Register />
             </Route>
-            <Route path="/">
+            <Route exact path="/">
               <Dashboard />
+            </Route>
+            <Route exact path="/creator">
+              <ItineraryCreator />
             </Route>
           </Switch>
         </div>
