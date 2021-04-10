b095bdfea97396e05ec98f11312e29e8e03a2f0b
sossw1 <sossw1@gmail.com>
2021-04-10 11:17:06 -0400

    Create a user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,5 +13,11 @@ module.exports = {
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 };
