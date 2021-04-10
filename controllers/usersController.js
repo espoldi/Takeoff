c7d4a6efc119026cd11010e87e08bc41b0be08fb
sossw1 <sossw1@gmail.com>
2021-04-10 11:17:30 -0400

    Update a user's password

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,5 +19,11 @@ module.exports = {
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  updatePassword: function(req, res) {
+    db.User
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 };
