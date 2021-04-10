d25a7b37a5e8adb53ae6d816cc46bd90e66d1d40
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-10 16:34:43 -0400

    added grid formatting and date selector for adding itinerary items

--- client/src/pages/ItineraryEditor.js
+++ client/src/pages/ItineraryEditor.js
@@ -10,12 +10,17 @@ import {
     Collapse,
     Container,
     Divider,
+    FormControl,
+    FormHelperText,
     Grid,
+    InputLabel,
     List,
     ListItem,
     ListItemIcon,
     ListItemText,
     ListSubheader,
+    MenuItem,
+    Select,
     TextField,
     Typography
 } from '@material-ui/core';
@@ -31,11 +36,18 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
-      },
-      nested: {
+    },
+    nested: {
         paddingLeft: theme.spacing(4),
-      },
-    heading: { fontSize: theme.typography.pxToRem(15) }
+    },
+    heading: { fontSize: theme.typography.pxToRem(15) },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
 }));
 
 export default function ItineraryEditor() {
@@ -47,6 +59,11 @@ export default function ItineraryEditor() {
     const handleStartDateChange = (date) => { setSelectedStartDate(date); };
     const handleEndDateChange = (date) => { setSelectedEndDate(date); };
 
+    const [itemDate, setItemDate] = useState("");
+    const handleItemDateChange = (event) => {
+        setItemDate(event.target.value);
+    };
+
     return (
         <Container fixed>
             <h1>Itinerary Editor</h1>
@@ -57,43 +74,51 @@ export default function ItineraryEditor() {
                             <Typography className={classes.heading}>Basic Trip Settings</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-                            <TextField
-                                id="trip-name"
-                                label="Trip Name"
-                                variant="outlined"
-                            />
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        id="trip-name"
+                                        label="Trip Name"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        id="destination"
+                                        label="Destination"
+                                        variant="outlined"
+                                    />
+                                </Grid>
 
-                            <TextField
-                                id="destination"
-                                label="Destination"
-                                variant="outlined"
-                            />
-
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <KeyboardDatePicker
-                                    margin="normal"
-                                    id="starting-date"
-                                    label="Start Date"
-                                    format="MM/dd/yyyy"
-                                    value={selectedStartDate}
-                                    onChange={handleStartDateChange}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change start date',
-                                    }}
-                                />
-
-                                <KeyboardDatePicker
-                                    margin="normal"
-                                    id="ending-date"
-                                    label="End Date"
-                                    format="MM/dd/yyyy"
-                                    value={selectedEndDate}
-                                    onChange={handleEndDateChange}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change end date',
-                                    }}
-                                />
-                            </MuiPickersUtilsProvider>
+                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <Grid item xs={12}>
+                                        <KeyboardDatePicker
+                                            margin="normal"
+                                            id="starting-date"
+                                            label="Start Date"
+                                            format="MM/dd/yyyy"
+                                            value={selectedStartDate}
+                                            onChange={handleStartDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change start date',
+                                            }}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <KeyboardDatePicker
+                                            margin="normal"
+                                            id="ending-date"
+                                            label="End Date"
+                                            format="MM/dd/yyyy"
+                                            value={selectedEndDate}
+                                            onChange={handleEndDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change end date',
+                                            }}
+                                        />
+                                    </Grid>
+                                </MuiPickersUtilsProvider>
+                            </Grid>
                         </AccordionDetails>
                         <Divider />
                         <AccordionActions>
@@ -107,7 +132,27 @@ export default function ItineraryEditor() {
                             <Typography className={classes.heading}>Add New Itinerary Item</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-
+                            <Grid container>
+                                <Grid item xs={12}>
+                                    <FormControl variant="outlined" className={classes.formControl}>
+                                        <InputLabel id="Date-Selector">Date</InputLabel>
+                                        <Select
+                                            labelId="item-date"
+                                            id="item-date"
+                                            value={itemDate}
+                                            onChange={handleItemDateChange}
+                                            label="Date"
+                                        >
+                                            <MenuItem value="">
+                                                <em>None</em>
+                                            </MenuItem>
+                                            <MenuItem value={10}>Ten</MenuItem>
+                                            <MenuItem value={20}>Twenty</MenuItem>
+                                            <MenuItem value={30}>Thirty</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
                         </AccordionDetails>
                         <Divider />
                         <AccordionActions>
@@ -118,7 +163,7 @@ export default function ItineraryEditor() {
                 </Grid>
 
                 <Grid item xs={12} md={9}>
-                    
+                    {/* Insert function to make nested list items for each day in range of start and end dates */}
                 </Grid>
             </Grid>
         </Container>
