5fa06d978cf40e34eb0e88ba48a22b3768a35971
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-09 20:26:44 -0400

    added destination textfield

--- client/src/pages/ItineraryCreator.js
+++ client/src/pages/ItineraryCreator.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 // Components
 import Navbar from '../components/Navbar.js';
 // Material UI
-import { Container, Grid } from '@material-ui/core';
+import {
+    Container,
+    Grid,
+    TextField
+} from '@material-ui/core';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
@@ -19,32 +23,41 @@ export default function ItineraryCreator() {
             <Navbar />
             <Container fixed>
                 <form>
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <Grid container justify="space-around">
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="starting-date"
-                                label="Start Date"
-                                format="MM/dd/yyyy"
-                                value={selectedStartDate}
-                                onChange={handleStartDateChange}
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change start date',
-                                }}
-                            />
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="ending-date"
-                                label="End Date"
-                                format="MM/dd/yyyy"
-                                value={selectedEndDate}
-                                onChange={handleEndDateChange}
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change end date',
-                                }}
-                            />
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <TextField id="destination" label="Destination" variant="outlined" />
                         </Grid>
-                    </MuiPickersUtilsProvider>
+
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <Grid item xs={12}>
+                                <KeyboardDatePicker
+                                    margin="normal"
+                                    id="starting-date"
+                                    label="Start Date"
+                                    format="MM/dd/yyyy"
+                                    value={selectedStartDate}
+                                    onChange={handleStartDateChange}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change start date',
+                                    }}
+                                />
+                            </Grid>
+                            
+                            <Grid item xs={12}>
+                                <KeyboardDatePicker
+                                    margin="normal"
+                                    id="ending-date"
+                                    label="End Date"
+                                    format="MM/dd/yyyy"
+                                    value={selectedEndDate}
+                                    onChange={handleEndDateChange}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change end date',
+                                    }}
+                                />
+                            </Grid>
+                        </MuiPickersUtilsProvider>
+                    </Grid>
                 </form>
             </Container>
         </>
