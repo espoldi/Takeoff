0314f211e638a75855c9a522f8b6419f137b8e18
amo02008 <adrienne.osorio@gmail.com>
2021-04-10 21:08:04 -0400

    updated navbar functionality and added icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,145 +1,90 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
+import React, {useState} from "react";
+// import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { NavLink} from "react-router-dom";
+
 // Material UI
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import LeftMenuSlider from "@material-ui/core/Drawer"
 import {
   AppBar,
   CssBaseline,
   Divider,
-  Drawer,
   IconButton,
   List,
   ListItem,
+  ListItemIcon,
   ListItemText,
   Toolbar,
   Typography,
+  Box,
 } from "@material-ui/core";
+
 import FlightTakeoffIcon from "@material-ui/icons/FlightTakeoff";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import{
+  // ChevronLeftIcon,
+  Dashboard,
+  Description,
+  Image,
+  Queue,
+  Group,
+  ContactMail,
+  CancelPresentation
+} from "@material-ui/icons/";
 
-const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
+/// CSS Styles
+const useStyles =makeStyles ((theme) => ({
+  menuSliderContainer: {
+    width: 240,
+    height: "100%"
   },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-  link: {
+
+  link: 
+  {
     textDecoration: "none",
     color: theme.palette.text.primary,
-  },
+  }
 }));
 
-export default function Navbar() {
+
+  const menuItems = [
+     
+  {
+    ListItemIcon: <Group/>,
+    ListItemText: "Meet the Team",
+    ListItemPath: "/meet-the-team"
+  },
+  {
+    ListItemIcon: <ContactMail/>,
+    ListItemText: "Contact Us",
+    ListItemPath: "/contact"
+  },
+  {
+    ListItemIcon: <CancelPresentation/>,
+    ListItemText: "Log Off",
+    ListItemPath: "/"
+  },
+];
+const Navbar = () => {
+  const [state, setState] = useState({
+    left:false
+  });
+
+  const toggleSlider = (slider, open) => () => {
+    setState({...state, [slider]: open });
+  };
+
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
-          >
-            <FlightTakeoffIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Takeoff
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <List>
-          <NavLink to="/" className= { classes.link }>
+  
+  const sideList = (slider) => (
+    <Box className={classes.menuSliderContainer}
+    component="div"
+    onClick={toggleSlider(slider, false)}>
+      <List>
+      <NavLink to="/" className= { classes.link }>
             <ListItem button>
+              <ListItemIcon><Image/></ListItemIcon>
               <ListItemText primary={"Dashboard"} />
             </ListItem>
           </NavLink>
@@ -147,18 +92,23 @@ export default function Navbar() {
           <NavLink
             to="/creator" className= { classes.link }              >
             <ListItem button>
+              <ListItemIcon>
+                <Description/>
+              </ListItemIcon>
               <ListItemText primary="Itinerary Creator" />
             </ListItem>
           </NavLink>
           
           <NavLink to="/blog" className={classes.link}>
             <ListItem button>
+              <ListItemIcon><Dashboard/></ListItemIcon>
               <ListItemText primary={"My Blog"} />
             </ListItem>
           </NavLink>
           
           <NavLink to="/bucket-list" className={classes.link}>
             <ListItem button>
+              <ListItemIcon><Queue/></ListItemIcon>
               <ListItemText primary={"Bucket List"} />
             </ListItem>
           </NavLink>
@@ -167,30 +117,50 @@ export default function Navbar() {
           
           <NavLink to="/meet-the-team" className={classes.link}>
             <ListItem button>
+              <ListItemIcon><Group/></ListItemIcon>
               <ListItemText primary={"Meet the Team"} />
             </ListItem>
           </NavLink>
           
           <NavLink to="/contact" className={classes.link}>
             <ListItem button>
+              <ListItemIcon><ContactMail/></ListItemIcon>
               <ListItemText primary={"Contact Us"} />
             </ListItem>
           </NavLink>
-          {/* <NavLink to="/" className={classes.link}> THIS NEEDS TO ROUTE TO LOG OFF */}
           <ListItem button>
-            <ListItemText primary={"Log Off"} />
+            <ListItemIcon><CancelPresentation/></ListItemIcon>
+            <ListItemText>
+              Log Off
+            </ListItemText>
           </ListItem>
-          {/* </NavLink> */}
         </List>
-      </Drawer>
-
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: open,
-        })}
-      >
-        <div className={classes.drawerHeader} />
-      </main>
-    </div>
+    </Box>
   );
-}
+  return (
+   <Box component="nav">
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" onClick={toggleSlider("left", true)}>
+          <FlightTakeoffIcon/>
+        </IconButton>
+        <Typography variant="h5">
+          <Link to="/" style={{textDecoration: "none", color: "white" }}>
+          Takeoff
+          </Link>
+        </Typography>
+        <LeftMenuSlider
+        anchor="left"
+        open={state.left}
+        onClose={toggleSlider("left", false)}>
+          {sideList("left")}
+        </LeftMenuSlider>
+      </Toolbar>
+    </AppBar>
+  </Box>
+  )  
+};
+
+  
+
+export default Navbar;
