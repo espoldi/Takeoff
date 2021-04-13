55b86d297429fbf7fe811e437c52fe87a5f1c2a4
Nardin Lachowski <nardin@live.com>
2021-04-13 14:30:03 -0400

    created Posts component to display all blog posts

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// Material UI
+import { Container } from '@material-ui/core';
+
+export default function Post() {
+    return (
+        <Container fixed>
+            <h1>POST</h1>
+        </Container>
+    );
+}
