0448b55e35f1c1cdb263427d95d31899b96ca493
Nardin Lachowski <nardin@live.com>
2021-04-13 14:06:36 -0400

    defined methods required for the blog routes in the controller folder

---
+++ controllers/postsController.js
@@ -0,0 +1,34 @@
+//const db = require('../models');
+const Posts = require('../models/schemas/Posts');
+
+//Defining methods for the postsController
+module.exports = {
+  findAll: function (req, res) {
+    Posts.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    Posts.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    Posts.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    Posts.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    Posts.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
+  
