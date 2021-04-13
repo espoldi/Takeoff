4c3c493b420945065b05d739a40268b74f65f646
Nardin Lachowski <nardin@live.com>
2021-04-13 14:31:40 -0400

    starting to build the Blog page to include the pform, post, and posts components

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,11 +1,31 @@
 import React from 'react';
 // Material UI
-import { Container } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from '../components/Posts/Posts';
+import Form from '../components/Form/Form';
+import useStyles from './styles';
 
 export default function Blog() {
-    return (
-        <Container fixed>
+    const classes = useStyles();
 
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color ="inherit">
+                <Typography className={classes.heading} variant="h2" align="center"> Blog </Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
