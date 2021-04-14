3a623436cd4067ad510d461e0c4054780c2d2225
sossw1 <sossw1@gmail.com>
2021-04-13 20:00:59 -0400

    create adds a trip to user

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -6,5 +6,13 @@ module.exports = {
       .then(user => res.json(user.trips))
       .catch(err => res.status(422).json(err));
   },
-  
+  create: function (req, res) {
+    db.User.findById(req.params.id)
+      .then(user => {
+        user.trips.push(req.body);
+        user.save();
+        res.json(user.trips);
+      })
+      .catch(err => res.status(422).json(err));
+  }
 }
