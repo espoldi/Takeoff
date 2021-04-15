84fcde03f38c7df9e5ab20ecb5c77de30ca488ab
Nardin Lachowski <nardin@live.com>
2021-04-15 12:04:40 -0400

    adding to prop drilling to pass the setCurrentId to Post and updated the star icon on page

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import StarIcon from '@material-ui/icons/Star';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost, likePost } from '../../../actions/posts'
+import { deletePost, starPost } from '../../../actions/posts'
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
@@ -21,7 +21,7 @@ const Post = ({ post }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{color: 'white'}} size="small" onClick={()=> {}}>
+                <Button style={{color: 'white'}} size="small" onClick={()=> { setCurrentId(post._id)}} >
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
@@ -33,10 +33,10 @@ const Post = ({ post }) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=> dispatch(likePost(post._id))}>
-                    <ThumbUpAltIcon fontSize="small" />
-                    &nbsp; Like &nbsp;
-                    {post.likeCount}
+                <Button size="small" color="primary" onClick={()=> dispatch(starPost(post._id))}>
+                    <StarIcon fontSize="small" />
+                    &nbsp; &nbsp;
+                    {post.rating}
                 </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
