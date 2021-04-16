2e8d3a1e9e4ad8e633fb65c19e2ff65a1e3d8838
sossw1 <sossw1@gmail.com>
2021-04-16 18:04:10 -0400

    Get / will findAll by trip id

---
+++ routes/api/itineraryItems.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const itineraryItemsController = require('../../controllers/itineraryItemsController');
+
+// Blog Post routes
+router
+  .route('/')
+  .get(itineraryItemsController.findAll);
+  // .post(itineraryItemsController.create);
+
+// router
+//   .route('/:id')
+//   .get(itineraryItemsController.findById)
+//   .patch(itineraryItemsController.update)
+//   .delete(itineraryItemsController.remove);
+
+module.exports = router;
