e1e4da2f5dc56ddfa264416498627d1992643db7
sossw1 <sossw1@gmail.com>
2021-04-16 18:03:03 -0400

    Added itinerary items route

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const postRoutes = require("./posts");
 const tripRoutes = require("./trips");
+const itineraryRoutes = require("./itineraryItems");
 
 router.use("/users", userRoutes);
 router.use("/trips", tripRoutes);
+router.use("/itinerary", itineraryRoutes);
 router.use("/posts", postRoutes);
 
 module.exports = router;
