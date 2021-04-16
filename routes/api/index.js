c77058a7bcc6aa93ede4f045cd8fb52cbb188a20
sossw1 <sossw1@gmail.com>
2021-04-16 18:02:35 -0400

    C:/Program Files/Git/api/posts added back

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 // Routes
 const userRoutes = require("./users");
-//const postRoutes = require("./posts");
+const postRoutes = require("./posts");
 const tripRoutes = require("./trips");
 
 router.use("/users", userRoutes);
 router.use("/trips", tripRoutes);
-//router.use("/posts", postRoutes);
+router.use("/posts", postRoutes);
 
 module.exports = router;
