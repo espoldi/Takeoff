71d1e2fcecfbabaa476ad86ede132baf9db046dc
sossw1 <sossw1@gmail.com>
2021-04-17 16:08:12 -0400

    Pulling data from form

--- client/src/pages/Creator.js
+++ client/src/pages/Creator.js
@@ -3,87 +3,122 @@ import React, { useState } from 'react';
 import Editor from './Editor.js';
 // Material UI
 import {
-    Button,
-    Container,
-    Grid,
-    TextField
+  Button,
+  Container,
+  Grid,
+  TextField
 } from '@material-ui/core';
 import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 
+import { useSelector } from 'react-redux';
+import { createTrip } from '../actions/tripActions';
+
 export default function ItineraryCreator() {
+  const userId = useSelector(state => (state.auth.user.id));
 
-    const [selectedStartDate, setSelectedStartDate] = useState(Date.now());
-    const [selectedEndDate, setSelectedEndDate] = useState(Date.now());
+  const [selectedStartDate, setSelectedStartDate] = useState(Date.now());
+  const [selectedEndDate, setSelectedEndDate] = useState(Date.now());
 
-    const handleStartDateChange = (date) => { setSelectedStartDate(date); };
-    const handleEndDateChange = (date) => { setSelectedEndDate(date); };
+  const handleStartDateChange = (date) => { setSelectedStartDate(date); };
+  const handleEndDateChange = (date) => { setSelectedEndDate(date); };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      ...values,
+      selectedStartDate,
+      selectedEndDate,
+      userId
+    }
+    console.log(newUser);
+    //   const newTrip = {...newTrip, userId};
+    //   createTrip(newTrip);
+  }
 
-    return (
-        <Container fixed>
-            <form>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <TextField
-                            id="trip-name"
-                            label="Trip Name"
-                            variant="outlined"
-                        />
-                    </Grid>
+  const handleInputChange = e => {
+    const {name, value} = e.target
+    setValues({...values, [name]: value})
+  }
 
-                    <Grid item xs={12}>
-                        <TextField
-                            id="destination"
-                            label="Destination"
-                            variant="outlined"
-                        />
-                    </Grid>
+  const [values, setValues] = useState({
+    name: '',
+    location: '',
+    // start: '',
+    // end: '',
+    userId: userId
+  });
 
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <Grid item xs={6}>
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="starting-date"
-                                label="Start Date"
-                                format="MM/dd/yyyy"
-                                value={selectedStartDate}
-                                onChange={handleStartDateChange}
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change start date',
-                                }}
-                            />
-                        </Grid>
+  return (
+    <Container fixed>
+      <form onSubmit={handleSubmit}>
+        <Grid container>
+          <Grid item xs={12}>
+            <TextField
+              id="trip-name"
+              label="Trip Name"
+              name="name"
+              value={values.name}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </Grid>
 
-                        <Grid item xs={6}>
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="ending-date"
-                                label="End Date"
-                                format="MM/dd/yyyy"
-                                value={selectedEndDate}
-                                onChange={handleEndDateChange}
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change end date',
-                                }}
-                            />
-                        </Grid>
-                    </MuiPickersUtilsProvider>
+          <Grid item xs={12}>
+            <TextField
+              id="destination"
+              label="Destination"
+              name="location"
+              value={values.location}
+              onChange={handleInputChange}
+              variant="outlined"
+            />
+          </Grid>
 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                        startIcon={<FlightTakeoffIcon />}
-                        onClick={() => {
-                            // Insert functionality to save form data above
-                            // Insert link to editor with saved data above
-                        }}>Takeoff</Button>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid item xs={6}>
+              <KeyboardDatePicker
+                margin="normal"
+                id="starting-date"
+                label="Start Date"
+                format="MM/dd/yyyy"
+                name="start"
+                value={selectedStartDate}
+                onChange={handleStartDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change start date',
+                }}
+              />
+            </Grid>
 
-                </Grid>
-            </form>
-        </Container>
-    );
+            <Grid item xs={6}>
+              <KeyboardDatePicker
+                margin="normal"
+                id="ending-date"
+                label="End Date"
+                format="MM/dd/yyyy"
+                name="end"
+                value={selectedEndDate}
+                onChange={handleEndDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change end date',
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            startIcon={<FlightTakeoffIcon />}
+            onClick={handleSubmit}
+          >Takeoff</Button>
+
+        </Grid>
+      </form>
+    </Container>
+  );
 }
