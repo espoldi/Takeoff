9c9de179e69408b162a9180698a484b420e294ae
sossw1 <sossw1@gmail.com>
2021-04-17 11:16:39 -0400

    Renamed file

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,56 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from './types/postTypes';
+import * as api from '../api';
+
+//Action Creators redux thunk=async 
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+
+        dispatch({ type: FETCH_ALL, payload: data });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: CREATE, payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: UPDATE, payload: data });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: DELETE, payload: id });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const starPost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.starPost(id);
+
+        dispatch({ type: UPDATE, payload: data });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
