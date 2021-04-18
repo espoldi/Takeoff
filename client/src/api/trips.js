1314b1152581e6921d8f4e93236e94eb0ca56f29
sossw1 <sossw1@gmail.com>
2021-04-18 13:12:33 -0400

    Fix axios get with params instead of body

--- client/src/api/trips.js
+++ client/src/api/trips.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url = 'http://localhost:3030/api/trips';
 
-export const fetchTrips = (userId) => axios.get(url, userId);
+export const fetchTrips = (query) => axios.get(url, { params: { query } });
 export const createTrip = (newTrip) => axios.post(url, newTrip);
 export const updatedTrip = (id, update) => axios.patch(`${url}/${id}`, update);
 export const deleteTrip = (id) => axios.delete(`${url}/${id}`);
