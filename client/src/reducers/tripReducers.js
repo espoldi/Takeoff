e513cfa9acc26c6879ce318d964484f412b4711c
sossw1 <sossw1@gmail.com>
2021-04-18 12:12:36 -0400

    State separated into current and archived trips, edited reducers to reflect this

--- client/src/reducers/tripReducers.js
+++ client/src/reducers/tripReducers.js
@@ -2,25 +2,34 @@ import { FETCH_ALL_TRIPS, CREATE_TRIP, UPDATE_TRIP, DELETE_TRIP }
   from '../actions/types/tripTypes';
 
 const initialState = {
-  currentTrip: {},
-  trips: []
+  workingTrip: {},
+  currentTrips: [],
+  archivedTrips: []
 }
 
 function tripReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_ALL_TRIPS:
-      return action.payload;
+      return {
+        ...state,
+        currentTrips: [
+          action.payload.currentTrips
+        ],
+        archivedTrips: [
+          action.payload.archivedTrips
+        ]
+      }
     case CREATE_TRIP:
       return {
         ...state,
-        currentTrip: action.payload,
-        trips: [
-          ...state.trips,
+        workingTrip: action.payload,
+        currentTrips: [
+          ...state.currentTrips,
           action.payload
         ]
       }
     case UPDATE_TRIP:
-      return state.map((trip) => (trip._id === action.payload._id ? action.payload: trip));
+      return state.map((trip) => (trip._id === action.payload._id ? action.payload : trip));
     case DELETE_TRIP:
       return state.filter((trip) => trip._id !== action.payload);
     default: 
