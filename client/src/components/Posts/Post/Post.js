593a97a185a9eda76a6bc485a245cd4726832602
Nardin Lachowski <nardin@live.com>
2021-04-18 18:25:28 -0400

    removed post rating functionality, added prop drilling into the post component, changed how we pass the post data

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,52 +1,42 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Tooltip } from '@material-ui/core';
-import StarIcon from '@material-ui/icons/Star';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost, starPost } from '../../../actions/postActions'
+import { deletePost, setCurrentPost } from '../../../actions/postActions'
 
-const Post = ({ post, setCurrentId }) => {
+const Post = (props) => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile || 'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'} title={post.title} />
+            <CardMedia className={classes.media} image={props.post.selectedFile || 'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'} title={props.post.title} />
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
-                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+                <Typography variant="body2">{moment(props.post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-            <Tooltip title="Edit" aria-label="edit">
-                <Button style={{color: 'white'}} size="small" onClick={()=> { setCurrentId(post._id)}} >
-                    <MoreHorizIcon fontSize="default" />
-                </Button>
+                <Tooltip title="Edit" aria-label="edit">
+                    <Button style={{ color: 'white' }} size="small" onClick={() => dispatch(setCurrentPost(props.post._id))}>
+                        <MoreHorizIcon fontSize="default" />
+                    </Button>
                 </Tooltip>
             </div>
             <div className={classes.details}>
-                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+                <Typography variant="body2" color="textSecondary">{props.post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
-            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
+            <Typography className={classes.title} variant="h5" gutterBottom>{props.post.title}</Typography>
             <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{props.post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Tooltip title="Star" aria-label="star">
-                <Button size="small" color="primary" onClick={()=> dispatch(starPost(post._id))}>
-                    <StarIcon fontSize="small" />
-                    &nbsp; &nbsp;
-                    {post.rating}
-                </Button>
-                </Tooltip>
                 <Tooltip title="Delete" aria-label="Delete">
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize="small" />
-                    Delete
-                </Button>
+                    <Button size="small" color="primary" onClick={() => dispatch(deletePost(props.post._id))}>
+                        <DeleteIcon fontSize="small" /> Delete
+                    </Button>
                 </Tooltip>
             </CardActions>
         </Card>
