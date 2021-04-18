249937a36649ff1629e14390e76779e9ebc62a9f
sossw1 <sossw1@gmail.com>
2021-04-17 20:57:06 -0400

    Fix types

--- client/src/actions/tripActions.js
+++ client/src/actions/tripActions.js
@@ -1,11 +1,11 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from './types/tripTypes';
+import { FETCH_ALL_TRIPS, CREATE_TRIP, UPDATE_TRIP, DELETE_TRIP } from './types/tripTypes';
 import * as api from '../api/trips';
 
 export const getTrips = () => async (dispatch) => {
   try {
     const { data } = await api.fetchTrips();
 
-    dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: FETCH_ALL_TRIPS, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -14,7 +14,7 @@ export const getTrips = () => async (dispatch) => {
 export const createTrip = (trip) => async (dispatch) => {
   try {
     const { data } = await api.createTrip(trip);
-    dispatch({ type: CREATE, payload: data })
+    dispatch({ type: CREATE_TRIP, payload: data })
   } catch (error) {
     console.log(error);
   }
