dffea45a36a28dcada36206433ff9f6b0d4a79a4
amo02008 <adrienne.osorio@gmail.com>
2021-04-18 18:39:48 -0400

    added titles to team description

--- client/src/pages/MeetTheTeam.js
+++ client/src/pages/MeetTheTeam.js
@@ -15,14 +15,19 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 const useStyles = makeStyles((theme) => ({
     root: { flexGrow: 1 },
     paper: {
-        padding: theme.spacing(2),
+        padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary
     },
+    
     pic: {
         width: theme.spacing(12),
         height: theme.spacing(12)
-    }
+    },
+    Avatar: {
+       justify:'center'
+    },
+    
 }));
 
 export default function MeetTheTeam() {
@@ -30,11 +35,14 @@ export default function MeetTheTeam() {
     return (
         <Container fixed>
             <div className={classes.root}>
-                <Grid container spacing={4}>
-                    <Grid item xs={6}>
+                <Grid container direction="row"  alignItems="center" spacing={4}>
+                    <Grid item xs={6}
+                     justify='center' >
                         <Paper className={classes.paper} elevation={3}>
                             <Avatar alt="Nardin Lachowski" src="https://avatars.githubusercontent.com/u/39885707?v=4" className={classes.pic} />
                             <Typography variant="h5" color="textSecondary">Nardin Lachowski</Typography>
+                            <Typography variant="h6" color="textSecondary">Web Developer</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">Back End</Typography>
                             <IconButton aria-label="Github" href="https://github.com/nard1n"><GithubIcon /></IconButton>
                             <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/in/nardinl"><LinkedInIcon /></IconButton>
                         </Paper>
@@ -43,6 +51,8 @@ export default function MeetTheTeam() {
                         <Paper className={classes.paper} elevation={3}>
                             <Avatar alt="Adrienne Osorio" src="https://avatars.githubusercontent.com/u/72840388?v=4" className={classes.pic} />
                             <Typography variant="h5" color="textSecondary">Adrienne Osorio</Typography>
+                            <Typography variant="h6" color="textSecondary">Web Developer</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">Front End</Typography>
                             <IconButton aria-label="Github" href="https://github.com/amo02008"><GithubIcon /></IconButton>
                             <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/in/adrienne-osorio-jrdev"><LinkedInIcon /></IconButton>
                         </Paper>
@@ -51,6 +61,8 @@ export default function MeetTheTeam() {
                         <Paper className={classes.paper} elevation={3}>
                             <Avatar alt="Bill Soss" src="https://avatars.githubusercontent.com/u/58079883?v=4" className={classes.pic} />
                             <Typography variant="h5" color="textSecondary">Bill Soss</Typography>
+                            <Typography variant="h6" color="textSecondary">Web Developer</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">Back End</Typography>
                             <IconButton aria-label="Github" href="https://github.com/sossw1"><GithubIcon /></IconButton>
                             <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/in/william-soss/"><LinkedInIcon /></IconButton>
                         </Paper>
@@ -59,6 +71,8 @@ export default function MeetTheTeam() {
                         <Paper className={classes.paper} elevation={3}>
                             <Avatar alt="Emily Spoldi" src="https://avatars.githubusercontent.com/u/72423431?v=4" className={classes.pic} />
                             <Typography variant="h5" color="textSecondary">Emily Spoldi</Typography>
+                            <Typography variant="h6" color="textSecondary">Web Developer</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">Front End</Typography>
                             <IconButton aria-label="Github" href="https://github.com/espoldi"><GithubIcon /></IconButton>
                             <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/in/emily-spoldi/"><LinkedInIcon /></IconButton>
                         </Paper>
@@ -67,6 +81,8 @@ export default function MeetTheTeam() {
                         <Paper className={classes.paper} elevation={3}>
                             <Avatar alt="Adriana Valdiglesias" src="https://avatars.githubusercontent.com/u/73134344?v=4" className={classes.pic} />
                             <Typography variant="h5" color="textSecondary">Adriana Valdiglesias</Typography>
+                            <Typography variant="h6" color="textSecondary">Web Developer</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">Front End</Typography>
                             <IconButton aria-label="Github" href="https://github.com/adrianavv1"><GithubIcon /></IconButton>
                             <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/in/adriana-valdiglesias-962677184/"><LinkedInIcon /></IconButton>
                         </Paper>
