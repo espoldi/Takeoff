9e4e8ad2257f53b81dc414819c008d9318bf9ad0
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-19 14:03:52 -0400

    erased paper formatting

--- client/src/pages/Editor.js
+++ client/src/pages/Editor.js
@@ -4,8 +4,7 @@ import EditorMenu from '../components/EditorMenu.js';
 import Itinerary from '../components/Itinerary.js';
 // Material UI
 import {
-    Container,
-    Grid
+    Container
 } from '@material-ui/core';
 
 export default function ItineraryEditor() {
@@ -13,15 +12,8 @@ export default function ItineraryEditor() {
 
     return (
         <Container fixed>
-            <h1>Itinerary Editor</h1>
-            <Grid container>
-                <Grid item xs={12} md={4}>
-                    <EditorMenu />
-                </Grid>
-                <Grid item xs={12} md={8}>
-                    <Itinerary />
-                </Grid>
-            </Grid>
+            <EditorMenu />
+            <Itinerary />
         </Container>
     );
 }
