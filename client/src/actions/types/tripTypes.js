862ba99b5df655b5eb9e8076d0a378d95d8e744f
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-04-18 20:33:28 -0400

    Added SET_CURRENT_TRIP type

--- client/src/actions/types/tripTypes.js
+++ client/src/actions/types/tripTypes.js
@@ -2,3 +2,4 @@ export const CREATE_TRIP = 'CREATE_TRIP';
 export const UPDATE_TRIP = 'UPDATE_TRIP';
 export const DELETE_TRIP = 'DELETE_TRIP';
 export const FETCH_ALL_TRIPS = 'FETCH_ALL_TRIPS';
+export const SET_CURRENT_TRIP = 'SET_CURRENT_TRIP';
