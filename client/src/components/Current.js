279c56acbd8f051cefd193e25a4ccf19549f0c4c
amo02008 <adrienne.osorio@gmail.com>
2021-04-19 13:07:03 -0400

    added gutter to heading for more space between trip cards, added margin to top of heading to allow for more space beneath navbar

--- client/src/components/Current.js
+++ client/src/components/Current.js
@@ -4,26 +4,33 @@ import TripCard from '../components/TripCard.js';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper, Typography } from '@material-ui/core';
 
+
 const useStyles = makeStyles((theme) => ({
+    
     root: { flexGrow: 1 },
     paper: {
         padding: theme.spacing(2),
+        marginTop:'50px',
         textAlign: 'center',
-        color: theme.palette.text.secondary
+        color: theme.palette.text.secondary,
+        width:'100%'
+        
     }
 }));
 
 function Current(props) {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <Paper className={classes.paper}>
-                <Grid item xs={12}>
-                    <Typography variant="h5" color="textSecondary">My Current Adventures</Typography>
+        
+        <div className={classes.root} >
+            
+            <Paper className={classes.paper} >
+                <Grid item xs={12} >
+                    <Typography variant="h4" color="textSecondary"gutterBottom="true">My Current Adventures</Typography>
                 </Grid>
-                <Grid container spacing={2}>
+                <Grid container spacing={4} >
                     {props.data.map((trip) => (
-                        <Grid item xs={12} md={6} lg={4}>
+                        <Grid item xs={12} md={6} lg={4} >
                             <TripCard data={trip} />
                         </Grid>
                     ))}
